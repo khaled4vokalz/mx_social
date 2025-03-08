26425e66d8e3c736721d2285d0123feb291ec2b4
maruf <maruf.khaled@gmail.com>
2025-03-08 16:29:38 +0600

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import ModeToggle from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton></UserButton>
-      </SignedIn>
-      <ModeToggle></ModeToggle>
-    </div>
-  );
+  return <div className="m-4">Home Page COntent</div>;
 }

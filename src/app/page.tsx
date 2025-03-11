094a9c6d663b3aa664db15ebe750bacca8f7c630
maruf <maruf.khaled@gmail.com>
2025-03-11 23:58:53 +0600

    FOLLOW feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CreatePost from "@/components/create-post";
+import WhoToFollow from "@/components/who-to-follow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
@@ -7,7 +8,9 @@ export default async function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
-      <div className="hidden lg:block lg:col-span-4">WhoToFollow</div>
+      <div className="hidden lg:block lg:col-span-4">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }

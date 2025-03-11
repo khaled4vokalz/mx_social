3d64d3a7d50880d2af2957c2b4420a69b5cda697
maruf <maruf.khaled@gmail.com>
2025-03-11 23:08:36 +0600

    add create POST feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div className="m-4">Home Page COntent</div>;
+import CreatePost from "@/components/create-post";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const user = await currentUser();
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      <div className="hidden lg:block lg:col-span-4">WhoToFollow</div>
+    </div>
+  );
 }
